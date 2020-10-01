bceaecc680b9e3bb0b406f348750dd73d2576cb1
Zachary Durr <zdurr22@gmail.com>
2020-10-01 16:32:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else
-    return array[0..-2].join(', ') + ", and " array[-1]
+    return array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
